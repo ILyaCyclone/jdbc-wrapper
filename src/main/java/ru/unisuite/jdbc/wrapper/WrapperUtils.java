f5733234b596bdba5d2fa677dcfd25b2bee57508
Adusya <druna007@rambler.ru>
2019-01-09 13:37:23 +0300

    add class WrapperUtils with reExecutionRequired static method

---
+++ src/main/java/ru/unisuite/jdbc/wrapper/WrapperUtils.java
@@ -0,0 +1,14 @@
+package ru.unisuite.jdbc.wrapper;
+
+import java.sql.SQLException;
+
+public class WrapperUtils {
+	
+	private static final int ERROR_CODE = 4068;
+	private static final String SQL_STATE = "72000";
+
+	public static boolean reExecutionRequired(SQLException e) {
+		return e.getErrorCode() == ERROR_CODE && SQL_STATE.equals(e.getSQLState());
+	}
+	
+}
